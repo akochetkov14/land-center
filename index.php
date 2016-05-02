1729b8ec2146577155d07082e84baf7c396c060a
dmitrygrishin <dmitry.grishin@me.com>
2016-05-02 20:12:02 +0300

    Удалил комментарии

--- index.php
+++ index.php
@@ -2,9 +2,15 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="<?php bloginfo('charset') ?>">
     <title>Document</title>
 </head>
 <body>
     
 </body>
 </html>
+</html>
+
